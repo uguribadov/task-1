631e4ffd59d03d6fd9107c17a8c6b3629ae368d5
uguribadov <uguribadov2012@gmail.com>
2026-03-01 13:56:11 +0400

    publish

---
+++ app.js
@@ -0,0 +1,18 @@
+// let name = prompt("Adınızı girin:");
+// let greet = "salam, " + name + "!";
+// alert(greet);
+// console.log(greet);
+
+let fullname = "Ugur Ibadov";
+console.log(fullname.charAt(0));
+console.log(fullname.charAt(5));
+
+let fullName = "Ugur Ibadov";
+ console.log(fullName.slice(5), fullName.slice(0, 4));
+
+let email = prompt("Emailinizi daxil edin: "); //uguribadov2012@gmail.com
+console.log(email.slice(email.indexOf("@") + 1));
+
+let wrd = "salam dunya";
+console.log(wrd.includes("dunya") + " | " + "true = bu demekdirki bele bir sey var");
+
