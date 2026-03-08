15a38faf184b147a9684e9981ead55fc212ef326
uguribadov <uguribadov2012@gmail.com>
2026-03-08 14:14:30 +0400

    commit just commmit

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-// let name = prompt("Adınızı girin:");
-// let greet = "salam, " + name + "!";
-// alert(greet);
-// console.log(greet);
+// // let name = prompt("Adınızı girin:");
+// // let greet = "salam, " + name + "!";
+// // alert(greet);
+// // console.log(greet);
 
-let fullname = "Ugur Ibadov";
-console.log(fullname.charAt(0));
-console.log(fullname.charAt(5));
+// // const { log } = require("console");
 
-let fullName = "Ugur Ibadov";
- console.log(fullName.slice(5), fullName.slice(0, 4));
+// // let fullname = "Ugur Ibadov";
+// // console.log(fullname.split(" "));
 
-let email = prompt("Emailinizi daxil edin: "); //uguribadov2012@gmail.com
-console.log(email.slice(email.indexOf("@") + 1));
 
-let wrd = "salam dunya";
-console.log(wrd.includes("dunya") + " | " + "true = bu demekdirki bele bir sey var");
+// // let fullName = "Ugur Ibadov";
+// //  console.log(fullName.slice(5), fullName.slice(0, 4));
 
+// // let email = prompt("Emailinizi daxil edin: "); //uguribadov2012@gmail.com
+// // console.log(email.slice(email.indexOf("@") + 1));
+
+// // let wrd = "salam dunya";
+// // console.log(wrd.includes("dunya"));
+
+
+// let frst = prompt("Birinci ədədi daxil edin: ");
+// let scnd = prompt("İkinci ədədi daxil edin: ");
+
+// console.log(Number(frst) + Number(scnd));
+// console.log(Number(frst) - Number(scnd));
+// console.log(Number(frst) * Number(scnd));
+// console.log(Number(frst) / Number(scnd));
+
+
+// let hsl = 12345 * 98765;
+// console.log(hsl);
+// console.log(hsl.toString()[5]);
+// ;
+
+
+
+let test = 123;
+console.log(test == 123)
+console.log(test > 1);
+console.log(test < 1);
+console.log(test >= 1);
+console.log(test <= 1);
+console.log(test == 1);
+console.log(test != 1);
+console.log(test === 1);
+console.log(test !== 1);
+console.log(test > 1 && test < 200);
+console.log(test > 1 || test < 1);
+console.log(!test);
+console.log(!!test);
