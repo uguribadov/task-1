77ee8bffb2a89b26f05c9e568456448a56ee8c80
uguribadov <uguribadov2012@gmail.com>
2026-03-10 20:27:10 +0400

    yes pls

--- app.js
+++ app.js
@@ -3,49 +3,62 @@
 // // alert(greet);
 // // console.log(greet);
 
+// bimbimbambam
+
 // // const { log } = require("console");
+
+// bimbimbambam
 
 // // let fullname = "Ugur Ibadov";
 // // console.log(fullname.split(" "));
 
+// bimbimbambam
 
 // // let fullName = "Ugur Ibadov";
 // //  console.log(fullName.slice(5), fullName.slice(0, 4));
 
+// bimbimbambam
+
 // // let email = prompt("Emailinizi daxil edin: "); //uguribadov2012@gmail.com
 // // console.log(email.slice(email.indexOf("@") + 1));
+
+// bimbimbambam
 
 // // let wrd = "salam dunya";
 // // console.log(wrd.includes("dunya"));
 
+// bimbimbambam
 
 // let frst = prompt("Birinci ədədi daxil edin: ");
 // let scnd = prompt("İkinci ədədi daxil edin: ");
+
+// bimbimbambam
 
 // console.log(Number(frst) + Number(scnd));
 // console.log(Number(frst) - Number(scnd));
 // console.log(Number(frst) * Number(scnd));
 // console.log(Number(frst) / Number(scnd));
 
+// bimbimbambam
 
 // let hsl = 12345 * 98765;
 // console.log(hsl);
 // console.log(hsl.toString()[5]);
 // ;
 
+// bimbimbambam
 
-
-let test = 123;
-console.log(test == 123)
-console.log(test > 1);
-console.log(test < 1);
-console.log(test >= 1);
-console.log(test <= 1);
-console.log(test == 1);
-console.log(test != 1);
-console.log(test === 1);
-console.log(test !== 1);
-console.log(test > 1 && test < 200);
-console.log(test > 1 || test < 1);
-console.log(!test);
-console.log(!!test);
+// let test = 123;
+// console.log(test == 123)
+// console.log(test > 1);
+// console.log(test < 1);
+// console.log(test >= 1);
+// console.log(test <= 1);
+// console.log(test == 1);
+// console.log(test != 1);
+// console.log(test === 1);
+// console.log(test !== 1);
+// console.log(test > 1 && test < 200);
+// console.log(test > 1 || test < 1);
+// console.log(!test);
+// console.log(!!test);
